e77ded52b2f24fd007e6ab78a031eb48c54a66df
yankaili2006 <yankaili2006@163.com>
2017-01-14 07:33:13 +0800

    jar-with-dependency

--- run.sh
+++ run.sh
@@ -6,12 +6,10 @@ APP=xwl-cmpp-client
 VERSION=1.0-SNAPSHOT
 JAR=$APP-$VERSION-jar-with-dependencies.jar
 
-JAR_PATH=$(curl "http://101.200.176.31:8181/nexus/service/local/artifact/maven/resolve?r=snapshots&g=com.xunwulian&a=$APP&v=$VERSION" | grep repositoryPath | sed 's/\s*<[^>]*>//g')
+JAR_PATH=$(curl "http://101.200.176.31:8181/nexus/service/local/artifact/maven/resolve?r=snapshots&g=com.xunwulian&a=$APP&v=$VERSION&c=jar-with-dependencies" | grep repositoryPath | sed 's/\s*<[^>]*>//g')
 
 NEXUS="http://101.200.176.31:8181/nexus/content/repositories/snapshots/$JAR_PATH"
 
 wget $NEXUS -O $JAR
 
-exit
-
-java -jar $JAR -a zxtd
+nohup java -jar $JAR -a zxtd &
